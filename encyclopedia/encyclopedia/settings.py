d63c4580004aa3dd1183061e38fd6cb0cdeda293
Rom Stevens <romney.stevens@gmail.com>
2015-11-22 16:49:12 -0600

    Convert to postgres database for heroku

--- encyclopedia/encyclopedia/settings.py
+++ encyclopedia/encyclopedia/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -87,8 +88,7 @@ WSGI_APPLICATION = 'encyclopedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'EncyclopediaData.sqlite3'),
+        dj_database_url.config()
     }
 }
 
