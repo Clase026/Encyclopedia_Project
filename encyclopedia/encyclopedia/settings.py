928e21e8a8a5e2354c256cf8edeafa7750855004
Rom Stevens <romney.stevens@gmail.com>
2015-11-22 16:59:48 -0600

    Fix database settings url

--- encyclopedia/encyclopedia/settings.py
+++ encyclopedia/encyclopedia/settings.py
@@ -87,11 +87,7 @@ WSGI_APPLICATION = 'encyclopedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        dj_database_url.config()
-    }
-}
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Internationalization
